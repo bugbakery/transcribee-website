cfb91a503cafc875a40ce16a6be9f5c133e2ed92
Philipp Mandler <info@philipp-mandler.com>
2023-10-07 19:58:15 +0200

    Use en dash for title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: { absolute: "transcribee — The open source automatic transcription tool", template: "%s — transcribee" },
+  title: { absolute: "transcribee – The open source automatic transcription tool", template: "%s — transcribee" },
   openGraph: {
-    title: { absolute: "transcribee — The open source automatic transcription tool", template: "%s — transcribee" },
+    title: { absolute: "transcribee – The open source automatic transcription tool", template: "%s — transcribee" },
     description: 'The open source automatic transcription tool',
     siteName: "transcribee",
   },
